3cfd5167586841ac369399ca058ad8862a2c359c
aki2314 <akihisa.11h@gmail.com>
2022-06-28 02:42:38 +0900

    パスワードの制限とバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         validates :nickname, presence: true
+         validates :firstname, presence: true
+         validates :familyname, presence: true
+         validates :firstname_two, presence: true
+         validates :familyname_two, presence: true
+         validates :birthday, presence: true
+
+
+
+
+
+
+
+
+
+
+
+        end
