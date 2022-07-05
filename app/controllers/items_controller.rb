054afc2328f597ec7cd9ce1e8d3f78459701f60e
aki2314 <akihisa.11h@gmail.com>
2022-07-06 00:48:42 +0900

    レビュー修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
 
   def index
   end
 
-  def new
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def create
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :firstname_two, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
