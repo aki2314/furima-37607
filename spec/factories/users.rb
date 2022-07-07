7c2d9cc0a600bee88a0db55f0984340b834ab761
aki2314 <akihisa.11h@gmail.com>
2022-07-07 14:28:27 +0900

    emailにFaker代入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { 'test' }
-    email                 { 'test@example' }
+    email                 { Faker::Internet.free_email }
     password              {'1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     firstname             { '有木' }
