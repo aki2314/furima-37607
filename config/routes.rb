7555a504fe151f2f229849bfea8a1f727cf2da8e
aki2314 <akihisa.11h@gmail.com>
2022-07-07 15:24:53 +0900

    LGTM後のroute修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'furimas#index'
+  root to: 'items#index'
 
-  resources :furimas, only: [ :index, :new, :create, :destroy ] do
+  resources :items, only: [ :index, :new, :create, :destroy ] do
   end
 end
