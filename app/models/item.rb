a921e9f375f8d3725c1b6e0f30cbca099153ae61
aki2314 <akihisa.11h@gmail.com>
2022-07-09 11:55:39 +0900

    active_hash項目作成とnewアクション序盤

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name,              presence: true
+  validates :detail,            presence: true
+  validates :category_id,       presence: true
+  validates :status_id,         presence: true
+  validates :shipmentsource_id, presence: true 
+  validates :price,             presence: true format: { with: /\A[0-9]+\z/ }
+  validates :derivalyprice_id,  presence: true
+  validates :derivalyday_id,    presence: true
+  validates :user,              presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipmentsource
+  belongs_to :derivalyprice
+  belongs_to :derivalyday
+
+  validates :title, :text, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
