41b5e793747b3b4a0c9bfc354c22cf887016c426
aki2314 <akihisa.11h@gmail.com>
2022-07-15 00:01:21 +0900

    商品一覧の情報表示とsoldoutの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
-    @item = Item.new
+    @item = Item.find(params[:id])
   end
 
   def create
@@ -21,5 +22,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :detail, :price, :image, :category_id, :status_id, :shipmentsource_id, :derivalyprice_id,
                                  :derivalyday_id).merge(user_id: current_user.id)
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
