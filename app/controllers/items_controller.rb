60f2d660364d67746f90fca586fcf9dbf678d8f1
aki2314 <akihisa.11h@gmail.com>
2022-07-16 13:58:29 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,3 @@ class ItemsController < ApplicationController
                                  :derivalyday_id).merge(user_id: current_user.id)
   end
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
-end
